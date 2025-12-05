f5d8aae033cfe3a879c87a3cff0576a25523d1f0
Eric Dufresne <ericmichaeldufresne@gmail.com>
2025-12-04 19:47:29 -0500

    Day 3

--- days/day3.py
+++ days/day3.py
@@ -1,5 +1,9 @@
 """
-
+Day 3. A persistent counter solves the joltage problem. Solved by creating a list of digits and comparing
+the digit. You then use a sliding window pointer to keep track of which digit you are comparing against. You
+have to be careful though as you run out of room for using digits from the bank as if you pick one, you need to make
+sure that there are enough consecutive entries in the bank to then fill the rest of the digits. Resetting anything to the
+right also ensures you don't improperly use a digit in places you shouldn't.
 """
 from utils import puzzle
 
@@ -25,16 +29,25 @@ def part2(puzzle_input):
     total = 0
     for bank in puzzle_input:
         max_digits = [0] * 12
-        left = 0
         for i, battery in enumerate(bank):
+            # You can only populate digits if you have enough consecutive batteries to then also fill the 12 digit
+            # number. This means you only iterate batteries you have room with the window closing as you iterate.
+            left = max(0, len(max_digits) - (len(bank) - i))
             val = int(battery)
-            if val > max_digits[left] and i != len(bank) - 12:
-                max_digits[left] = val
+            while left < len(max_digits):
+                if val > max_digits[left]:
+                    max_digits[left] = val
+                    if left < len(max_digits) - 1:
+                        # Must reset any digits to the right as taking a digit at this index means that
+                        # the joltage will always increase and you will therefore need to take the next
+                        # digit you find on the right as well.
+                        for j in range(left + 1, len(max_digits)):
+                            max_digits[j] = 0
+                    break
                 left += 1
-            elif val > second:
-                second = val
-        total += int(str(first) + str(second))
-        print(f'Bank: {bank}, max joltage:{int(str(first) + str(second))}')
+        joltage = int("".join([str(digit) for digit in max_digits]))
+        total += joltage
+        print(f'Bank: {bank}, max joltage:{joltage}')
     print(total)
 
 
