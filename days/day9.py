b31d9a3488f59eac20d411181ad452ae2eb2b829
Eric Dufresne <ericmichaeldufresne@gmail.com>
2025-12-10 01:01:26 -0500

    Half cooked ray casting impl

--- days/day9.py
+++ days/day9.py
@@ -25,7 +25,6 @@ def part1(puzzle_input):
     tile_positions = [
         tuple(int(val) for val in line.split(",")) for line in puzzle_input
     ]
-    print(tile_positions)
 
     area = 0
     for i, pos1 in enumerate(tile_positions):
@@ -35,123 +34,76 @@ def part1(puzzle_input):
             this_area = _area(pos1, pos2)
             if this_area > area:
                 area = this_area
-
     print(area)
 
 
 @puzzle
 def part2(puzzle_input):
-    tile_positions = [
-        tuple(int(val) for val in line.split(",")) for line in puzzle_input
-    ]
-    border_positions = {tile_positions[0]}
+    tile_positions = [tuple(int(val) for val in line.split(',')) for line in puzzle_input]
+    lines = []
     for i, tile_position in enumerate(tile_positions + [tile_positions[0]]):
-        border_positions.add(tile_position)
         last_position = tile_positions[i - 1]
-        if tile_position[0] == last_position[0]:
-            for j in range(
-                min(tile_position[1], last_position[1]),
-                max(tile_position[1], last_position[1]),
-            ):
-                border_positions.add((tile_position[0], j))
-        else:
-            for j in range(
-                min(tile_position[0], last_position[0]),
-                max(tile_position[0], last_position[0]),
-            ):
-                border_positions.add((j, tile_position[1]))
+        lines.append((last_position, tile_position))
 
-    area = 0
-    answer1, answer2 = None, None
+
+    rectangles = []
     for i, pos1 in enumerate(tile_positions):
-        for j, pos2 in enumerate(tile_positions):
-            if i == j:
+        for k, pos2 in enumerate(tile_positions):
+            if i == k:
                 continue
-            this_area = _area(pos1, pos2)
-            corner1, corner2 = (pos1[0], pos2[1]), (pos2[0], pos1[1])
-            if (
-                this_area > area
-                and _validate_border_positions(corner1, Pivot.from_pos(pos1, pos2, corner1), border_positions)
-                and _validate_border_positions(corner2, Pivot.from_pos(pos1, pos2, corner2), border_positions)
-            ):
-                area = this_area
-                answer1, answer2 = pos1, pos2
-        print(f'Testing: {i}, {j}')
 
-    print(f"{answer1} and {answer2}")
-    print(area)
+            area = _area(pos1, pos2)
+            rectangles.append((pos1, pos2, area))
 
-
-def _validate_border_positions(
-    starting_point: tuple[int, int],
-    pivot: Pivot,
-    border_positions: set[tuple[int, int]],
-) -> bool:
-    if starting_point in border_positions:
-        return True
-    if pivot == Pivot.TOP_LEFT or pivot == Pivot.TOP_RIGHT:
-        border_position_above = next((border_position for border_position in border_positions if border_position[0] == starting_point[0] and border_position[1] >= starting_point[1]), None)
-        if border_position_above is None:
-            return False
-    if pivot == Pivot.BOTTOM_LEFT or pivot == Pivot.BOTTOM_RIGHT:
-        border_position_below = next((border_position for border_position in border_positions if border_position[0] == starting_point[0] and border_position[1] <= starting_point[1]), None)
-        if border_position_below is None:
-            return False
-    if pivot == Pivot.TOP_LEFT or pivot == Pivot.BOTTOM_LEFT:
-        border_position_left = next((border_position for border_position in border_positions if border_position[1] == starting_point[1] and border_position[0] <= starting_point[1]), None)
-        if border_position_left is None:
-            return False
-    if pivot == Pivot.TOP_RIGHT or pivot == Pivot.BOTTOM_RIGHT:
-        border_position_right = next((border_position for border_position in border_positions if border_position[1] == starting_point[1] and border_position[0] >= starting_point[1]), None)
-        if border_position_right is None:
-            return False
-    return True
+    rectangles = sorted(rectangles, key=lambda x: x[2])
+    while rectangles:
+        pos1, pos2, area = rectangles.pop()
+        print(f'Original: {(pos1, pos2, area)}')
+        corner1, corner2 = (pos1[0], pos2[1]), (pos2[0], pos1[1])
+        print(f'Testing corners: {(corner1, corner2)}')
+        if (corner1 in tile_positions or _in_polygon(corner1, lines)) and (corner2 in tile_positions or _in_polygon(corner2, lines)):
+            print(area)
+            return
 
 
+def _in_polygon(pos, lines: list[tuple[tuple, tuple]]) -> bool:
+    print(f'Testing: {pos} in polygon')
+    for line in lines:
+        if _point_intersects(line, pos):
+            print(f'On edge of {line}')
+            return True
+    segments_to_trace = sorted([line for line in lines if line[0][0] == line[1][0] and min(line[0][1], line[1][1]) <= pos[1] < max(line[0][1], line[1][1])], key=lambda x: x[0][0])
+    rays = 0
+    last_dir = None
+    for segment in segments_to_trace:
+        min_y = min(segment[0][1], segment[1][1])
+        max_y = max(segment[0][1], segment[1][1])
+        if segment[0][1] == pos[1] and segment[0][1] == min_y or segment[1][1] == pos[1] and segment[1][1] == min_y:
+            # Hit bottom
+            current_dir = 'up'
+            if current_dir == last_dir:
+                rays += 1
+                last_dir = None
+            else:
+                last_dir = current_dir
+        elif segment[0][1] == pos[1] and segment[0][1] == max_y or segment[1][1] == pos[1] and segment[1][1] == max_y:
+            current_dir = 'down'
+            if current_dir == last_dir:
+                rays += 1
+                last_dir = None
+            else:
+                last_dir = current_dir
+        else:
+            last_dir = None
+            rays += 1
+    result = rays % 2 == 1
+    print(f'Ray traced: {rays}. Is in polygon: {result}')
+    return result
 
 
-# @puzzle
-# def part2(puzzle_input):
-#     tile_positions = [tuple(int(val) for val in line.split(',')) for line in puzzle_input]
-#     print(tile_positions)
-#     # Looking for missing 9,3 which should be between
-#     border_positions = {tile_positions[0]}
-#     for i, tile_position in enumerate(tile_positions + [tile_positions[0]]):
-#         border_positions.add(tile_position)
-#         last_position = tile_positions[i - 1]
-#         if tile_position[0] == last_position[0]:
-#             for j in range(min(tile_position[1], last_position[1]), max(tile_position[1], last_position[1])):
-#                 border_positions.add((tile_position[0], j))
-#         else:
-#             for j in range(min(tile_position[0], last_position[0]), max(tile_position[0], last_position[0])):
-#                 border_positions.add((j, tile_position[1]))
-#
-#     for i, pos1 in enumerate(tile_positions):
-#         for j, pos2 in enumerate(tile_positions):
-#             if i == j:
-#                 continue
-#             corner1, corner2 = (pos1[0], pos2[1]), (pos2[0], pos1[1])
-#             if corner1 in border_positions and corner2 in border_positions:
-#                 for x in range(min(corner1[0], corner2[0]), max(corner1[0], corner2[0]) + 1):
-#                     for y in range(min(corner1[1], corner2[1]), max(corner1[1], corner2[1]) + 1):
-#                         border_positions.add((x, y))
-#             print(f'i={i}, j={j}')
-#
-#     area = 0
-#     answer1, answer2 = None, None
-#     for i, pos1 in enumerate(tile_positions):
-#         for j, pos2 in enumerate(tile_positions):
-#             if i == j:
-#                 continue
-#             this_area = _area(pos1, pos2)
-#             corner1, corner2 = (pos1[0], pos2[1]), (pos2[0], pos1[1])
-#             if this_area > area and corner1 in border_positions and corner2 in border_positions:
-#                 area = this_area
-#                 answer1, answer2 = pos1, pos2
-#
-#     print(f'{answer1} and {answer2}')
-#     print(area)
-
+def _point_intersects(line: tuple[tuple, tuple], point: tuple[int, int]):
+    min_x, max_x, min_y, max_y = min(line[0][0], line[1][0]), max(line[0][0], line[1][0]), min(line[0][1], line[1][1]), max(line[0][1], line[1][1])
+    return min_x <= point[0] <= max_x and min_y <= point[1] <= max_y
 
 def _area(pos1, pos2):
     max_x = max(pos1[0], pos2[0])
