74e824ee2049ab6346c893fb649139cdcca0a735
Eric Dufresne <ericmichaeldufresne@gmail.com>
2025-12-15 21:49:23 -0500

    Day 11

--- days/day11.py
+++ days/day11.py
@@ -1,5 +1,11 @@
+"""
+I learned something new today. DFS didn't seem like the right solution because of having to find "all" paths and
+the tree size being huge. But you can use memoization which is when you cache answers to expensive questions. Doing
+the recursive DFS with a cache annotation in it will prevent unnecessary crawling through the tree.
+"""
 import collections
 from collections import defaultdict
+from functools import lru_cache
 
 from utils import puzzle
 
@@ -35,7 +41,6 @@ def bfs(m: dict, start: str, end: str) -> int:
 
 
 
-
 @puzzle
 def part2(puzzle_input):
     m = defaultdict(set)
@@ -43,29 +48,19 @@ def part2(puzzle_input):
         items = [item.strip() for item in line.split() if item.strip()]
         m[items[0].replace(':', '')] |= set(items[1:])
 
-    multipliers = defaultdict(lambda : 1)
-    while keys_to_replace := {k for k, v in m.items() if v == {'out'} and k not in ('out','svr', 'dac', 'fft')}:
-        print(f'Reducing: {keys_to_replace}')
-        for k, children in m.items():
-            children_to_replace = {child for child in children if child in keys_to_replace}
-            if children_to_replace:
-                multipliers[k] = sum(multipliers[child] for child in children_to_replace)
-                m[k] = {child for child in children if child not in children_to_replace} | {"out"}
-        for key_to_replace in keys_to_replace:
-            del m[key_to_replace]
+    @lru_cache(maxsize=None)
+    def dfs(node: str, dac: bool = False, fft: bool = False) -> int:
+        if node == 'out' and dac and fft:
+            return 1
+        total = 0
+        for child in m.get(node, []):
+            total += dfs(child, dac or child == 'dac', fft or child == 'fft')
 
+        return total
 
-    _print_map(m)
+    result = dfs('svr')
+    print(result)
 
-    result  = bfs(m, 'svr', 'fft')
-    result2 = bfs(m, 'svr', 'dac')
-    result3 = bfs(m, 'fft', 'dac')
-    result4 = bfs(m, 'dac', 'fft')
-    result5 = bfs(m, 'dac', 'out')
-    result6 = bfs(m, 'fft', 'out')
-    path1 = result * result3 * result5
-    path2 = result2 * result4 * result6
-    print(path1 + path2)
 
 
 def _print_map(m):
